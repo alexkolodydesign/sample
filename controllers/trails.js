0ffe9f3037bf2cdb4486d16110e4081bb3d73e7b
Alex Kolody <alexkolodydesign@gmail.com>
2018-07-26 07:16:35 -0600

    Use wordpress regions endpoint

--- controllers/trails.js
+++ controllers/trails.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const { regions } = require('../data/regions');
 
 exports.getTrailData = async (req, res) => {
   try {
@@ -19,8 +18,16 @@ exports.getTrailData = async (req, res) => {
 exports.getRegionData = async (req, res) => {
   try {
     const { data: trails } = await axios.get('http://washcotrails.flitchbeta.com/wp-json/wp/v2/trails?per_page=500&order=asc&region=6');
+    const { data: regions } = await axios.get('http://washcotrails.flitchbeta.com/wp-json/wp/v2/regions');
+    const regionsArray = regions.map((region) => ({
+      regionName: region.name,
+      markerIcon: region.custom_data.markerIcon,
+      markerCoordinates: { "lat": Number(region.custom_data.markerCoordinates.marker_latitude), "lng": Number(region.custom_data.markerCoordinates.marker_longitude) },
+      regionImage: region.custom_data.regionImage,
+      overlayImage: region.custom_data.region_background_image
+    }))
     return res.json({
-      regions,
+      regions: regionsArray,
       trails
     });
   } catch(e) {
