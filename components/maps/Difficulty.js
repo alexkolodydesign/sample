17520ab94b63f2714261fdd4dbf2c8ea93f9372a
Kaden McLaws <kadenmclaws@gmail.com>
2018-07-30 11:22:57 -0600

    Difficulty component complete

--- components/maps/Difficulty.js
+++ components/maps/Difficulty.js
@@ -1,48 +1,61 @@
-const Difficulty = props =>
-  <div>
-    <div className="difficulty">
-        {props.difficulty.map((type, k) => {
-          if (type == "defaultDifficulty") {
-            return
-              <p>Difficulty:
-                <img src={`/static/images/trail/difficulty/${type.value}.png`} alt={`${type.value} Difficulty`} />
-                <span>{type.label}</span>
-              </p>
-          } else if (type == "hikingDifficulty") {
-            return
-              <p>Hiking Difficulty:
-                <img src={`/static/images/trail/difficulty/${type.value}.png`} alt={`${type.value} Difficulty`} />
-                <span>{type.label}</span>
-              </p>
-          } else if (type == "bikingDifficulty") {
-            return
-              <p>Biking Difficulty:
-                <img src={`/static/images/trail/difficulty/${type.value}.png`} alt={`${type.value} Difficulty`} />
-                <span>{type.label}</span>
-              </p>
-          } else if (type == "equestrianDifficulty") {
-            return
-              <p>Equestrian Difficulty:
-                <img src={`/static/images/trail/difficulty/${type.value}.png`} alt={`${type.value} Difficulty`} />
-                <span>{type.label}</span>
-              </p>
-          } else if (type == "ohvDifficulty") {
-            return
-              <p>OHV Difficulty:
-                <img src={`/static/images/trail/difficulty/${type.value}.png`} alt={`${type.value} Difficulty`} />
-                <span>{type.label}</span>
-              </p>
-          } else {
-            return ""
+export default class Difficulty extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const difficulty = this.props.difficulty
+    return (
+      <div>
+        <div className="difficulty">
+          {difficulty.hikingDifficulty &&
+            <p>Hiking Difficulty:
+              <img src={`/static/images/trail/difficulty/${difficulty.hikingDifficulty.value}.png`} alt={`${difficulty.hikingDifficulty.value} Difficulty`} />
+              <span>{difficulty.hikingDifficulty.label}</span>
+            </p>
           }
-        })}
-    </div>
-    <style jsx>{`
-      .difficulty {
-        img {
-        }
-      }
-    `}</style>
-  </div>
+          {difficulty.bikingDifficulty &&
+            <p>Biking Difficulty:
+              <img src={`/static/images/trail/difficulty/${difficulty.bikingDifficulty.value}.png`} alt={`${difficulty.bikingDifficulty.value} Difficulty`} />
+              <span>{difficulty.bikingDifficulty.label}</span>
+            </p>
+          }
+          {difficulty.equestrianDifficulty &&
+            <p>Equestrian Difficulty:
+              <img src={`/static/images/trail/difficulty/${difficulty.equestrianDifficulty.value}.png`} alt={`${difficulty.equestrianDifficulty.value} Difficulty`} />
+              <span>{difficulty.equestrianDifficulty.label}</span>
+            </p>
+          }
+          {difficulty.ohvDifficulty &&
+            <p>OHV Difficulty:
+              <img src={`/static/images/trail/difficulty/${difficulty.ohvDifficulty.value}.png`} alt={`${difficulty.ohvDifficulty.value} Difficulty`} />
+              <span>{difficulty.ohvDifficulty.label}</span>
+            </p>
+          }
+          {difficulty.defaultDifficulty &&
+            <p>Difficulty:
+              <img src={`/static/images/trail/difficulty/${difficulty.defaultDifficulty.value}.png`} alt={`${difficulty.defaultDifficulty.value} Difficulty`} />
+              <span>{difficulty.defaultDifficulty.label}</span>
+            </p>
+          }
 
-  export default Difficulty
+        </div>
+        <style jsx>{`
+          .difficulty {
+            p {
+              margin: 3px 0 0;
+              font-weight: bold;
+            }
+            img {
+              margin: 0 3px 0 2px;
+              vertical-align: -2px;
+              max-height: 1.25rem;
+            }
+            span {
+              font-weight: normal;
+            }
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
