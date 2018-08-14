152634cf42fbe98075e09ceadadef5ce5370102b
Alex Kolody <alexkolodydesign@gmail.com>
2018-08-14 06:39:33 -0600

    Add polyfills for ie

--- components/layout/Head.js
+++ components/layout/Head.js
@@ -24,7 +24,7 @@ const HeadComponent = props => (
         content=""
       />
     )}
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.23.0/polyfill.min.js" />
     <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5M5G3B3');`}} />
 
     {/* Page view custom event - GTM */}
