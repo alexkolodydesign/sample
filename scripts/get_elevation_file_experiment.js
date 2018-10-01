674cf69e38db9a4cad26a2e9dc0b54032b2f3443
Alex Kolody <alexkolodydesign@gmail.com>
2018-10-01 08:29:05 -0600

    Script to get elevation from json. Performance wip

---
+++ scripts/get_elevation_file_experiment.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const axios = require('axios');
+
+run();
+
+// Time out to make sure Google's Elevation API doesn't receive too many requests (Max is 100 requests per second)
+const timeout = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+async function run() {
+
+  fs.readFile( './SuicidalVertices.json', async (err, data) => {
+    const json = JSON.parse(data)
+    const coordinates = json.features.map(feature => ({ lat: feature.attributes.POINT_Y, lng: feature.attributes.POINT_X }) )
+    const name = json.features[0].attributes["TRAIL_NAME"]
+    const filename = name.toLowerCase().replace(/ /g, "_")
+    const increments = Math.ceil( (coordinates.length / 200) )
+
+    for (let i = 0; i < coordinates.length; i++) {
+      const { data: { results: [ { elevation: elevation } ] } } = await axios.get(`https://maps.googleapis.com/maps/api/elevation/json?key=AIzaSyAqrxAbb0g9d1C9GgKjGZ5OU-TGowpZqWQ&locations=
+        ${coordinates[i].lat},${coordinates[i].lng}
+      `)
+      coordinates[i].elevation = elevation
+      await timeout(100);
+      console.log(`Point finished ${i} out of ${coordinates.length}`);
+    }
+
+    // Create final trail variable to store
+    const trail = { name, filename, coordinates }
+    // Write trail file
+    fs.writeFile(`./trails/${filename}.json`, trail, 'utf8', () => {
+      console.log(`Wrote ${filename} into a new file!`)
+    });
+
+   });
+}
