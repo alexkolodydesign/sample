1930aef5610d296e752336efb6524fedf4a39400
Jaimie Hayes <jaimie@flitchcreative.com>
2018-12-06 16:50:38 -0700

    Google Analytics, tag manager wip

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,13 +1,38 @@
-const Footer = props =>
-  <footer>
-    <div className="wrapper">
-      <p>Designed by Flitch Creative</p>
-    </div>
-    <style jsx>{`
-      footer {
-        display: none;
-      }
-    `}</style>
-  </footer>
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-MFQ7DJ4'
+};
+
+const injectGA = () => {
+  ReactGA.initialize('UA-130505850-1');
+  ReactGA.pageview(window.location.pathname);
+};
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    injectGA()
+    TagManager.initialize(tagManagerArgs)
+  }
+  render() {
+    return (
+      <footer>
+        <div className="wrapper">
+          <p>Designed by Flitch Creative</p>
+        </div>
+        <style jsx>{`
+          footer {
+            display: none;
+          }
+        `}</style>
+      </footer>
+    )
+  }
+}
 
 export default Footer
