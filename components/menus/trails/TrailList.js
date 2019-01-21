a9d44a6cc3f47a299fea625c46a92e9c189f72b8
Alex Kolody <akolody@worldventures.com>
2019-01-21 09:25:03 -0600

    Debugging navigation issues

--- components/menus/trails/TrailList.js
+++ components/menus/trails/TrailList.js
@@ -9,19 +9,41 @@ const TrailListMenu = dynamic(
   { loading: () => null, ssr: false }
 );
 
-const TrailList = ({ toggleMenus, trailsListMenu }) => (
-  <>
-    <TrailListButton
-      type="button"
-      onClick={() => toggleMenus(!trailsListMenu)}
-      className={trailsListMenu ? 'active trail_list' : 'trail_list'}
-    >
-      <img src="/static/images/menu/trail-list.svg" alt="Trail List" />
-      <p>Trail List</p>
-    </TrailListButton>
-    {trailsListMenu && <TrailListMenu />}
-  </>
-);
+// TODO: Redux is not updating this components prop correctly
+class TrailList extends React.Component {
+  state = { menu: this.props.trailsListMenu };
+
+  static getDerivedStateFromProps = (nextProps, nextState) => {
+    console.log({ nextProps, nextState });
+    return nextState;
+  };
+
+  toggleMenu = () => {
+    this.setState(state => {
+      this.props.toggleMenus(!state.menu);
+      return { menu: !state.menu };
+    });
+  };
+
+  render() {
+    const { menu } = this.state;
+    const { trailsListMenu } = this.props;
+    return (
+      <>
+        {console.log({ menu, trailsListMenu })}
+        <TrailListButton
+          type="button"
+          onClick={this.toggleMenu}
+          className={menu ? 'active trail_list' : 'trail_list'}
+        >
+          <img src="/static/images/menu/trail-list.svg" alt="Trail List" />
+          <p>Trail List</p>
+        </TrailListButton>
+        {menu && trailsListMenu && <TrailListMenu />}
+      </>
+    );
+  }
+}
 
 TrailList.propTypes = {
   toggleMenus: PropTypes.func.isRequired,
@@ -33,15 +55,17 @@ const mapStateToProps = state => ({
   trailsListMenu: state.map.menus.trailsListMenu
 });
 const mapDispatchToProps = dispatch => ({
-  toggleMenus: trailsListMenu =>
-    dispatch({
+  toggleMenus: trailsListMenu => {
+    console.log('DISPATCH: ', { trailsListMenu });
+    return dispatch({
       type: 'TOGGLE_MENUS',
       menus: {
         filterTrailsMenu: false,
         trailsListMenu,
         optionsMenu: false
       }
-    })
+    });
+  }
 });
 
 export default connect(
