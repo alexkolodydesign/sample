046d4106498337d03aa1c755d5e0375ddb1f65b5
Kaden McLaws <kaden@flitchcreative.com>
2019-01-25 11:03:57 -0700

    change URLS to match api and google bucket

--- controllers/events.js
+++ controllers/events.js
@@ -1,13 +1,14 @@
-const axios = require("axios");
-
+const axios = require('axios');
 
 exports.getEventData = async (req, res) => {
   try {
-    const { data: events } = await axios.get('http://washcotrails.flitchbeta.com/wp-json/wp/v2/washco_event?per_page=100');
+    const { data: events } = await axios.get(
+      'https://api.swutahtrails.com/wp-json/wp/v2/washco_event?per_page=100'
+    );
     // console.log(events)
     res.status(200).json(events);
-  } catch(e) {
-    console.log("Issue arose.", e);
-    res.status(500).send("Error")
+  } catch (e) {
+    console.log('Issue arose.', e);
+    res.status(500).send('Error');
   }
-}
+};
--- data/regions.js
+++ data/regions.js
@@ -1,43 +1,46 @@
 const regions = [
   {
     id: 3,
-    regionName: "Alpine",
-    markerIcon: "/static/images/regions/alpine-region.svg",
-    markerCoordinates: { "lat": 37.44084225, "lng": -113.620729 },
-    regionImage: "",
-    overlayImage: ""
+    regionName: 'Alpine',
+    markerIcon: '/static/images/regions/alpine-region.svg',
+    markerCoordinates: { lat: 37.44084225, lng: -113.620729 },
+    regionImage: '',
+    overlayImage: ''
   },
   {
     id: 5,
-    regionName: "Mesa",
-    markerIcon: "/static/images/regions/mesa-region.svg",
-    markerCoordinates: { "lat": 37.08483451, "lng": -113.1923863 },
-    regionImage: "",
-    overlayImage: ""
+    regionName: 'Mesa',
+    markerIcon: '/static/images/regions/mesa-region.svg',
+    markerCoordinates: { lat: 37.08483451, lng: -113.1923863 },
+    regionImage: '',
+    overlayImage: ''
   },
   {
     id: 6,
-    regionName: "Urban",
-    markerIcon: "/static/images/regions/urban-region.svg",
-    markerCoordinates: { "lat": 37.109891, "lng": -113.583317 },
-    regionImage: "",
-    overlayImage: "https://washcotrails.flitchbeta.com/wp-content/uploads/2018/07/urban_overlay-min.png"
+    regionName: 'Urban',
+    markerIcon: '/static/images/regions/urban-region.svg',
+    markerCoordinates: { lat: 37.109891, lng: -113.583317 },
+    regionImage: '',
+    overlayImage:
+      'https://storage.googleapis.com/swutahtrails/wordpress/2018/07/urban_overlay-min.png'
   },
   {
     id: 2,
-    regionName: "Canyon",
-    markerIcon: "/static/images/regions/canyon-region.svg",
-    markerCoordinates: { "lat": 37.32113432, "lng": -113.0664033 },
-    regionImage: "https://washcotrails.flitchbeta.com/wp-content/uploads/2018/07/canyon-2179250_1280-300x200.jpg",
-    overlayImage: "https://washcotrails.flitchbeta.com/wp-content/uploads/2018/07/canyon_overlay-min.png"
+    regionName: 'Canyon',
+    markerIcon: '/static/images/regions/canyon-region.svg',
+    markerCoordinates: { lat: 37.32113432, lng: -113.0664033 },
+    regionImage:
+      'https://storage.googleapis.com/swutahtrails/wordpress/2018/07/canyon-2179250_1280-300x200.jpg',
+    overlayImage:
+      'https://storage.googleapis.com/swutahtrails/wordpress/2018/07/canyon_overlay-min.png'
   },
   {
     id: 4,
-    regionName: "Desert",
-    markerIcon: "/static/images/regions/desert-region.svg",
-    markerCoordinates: { "lat": 37.19733874, "lng": -113.896597 },
-    regionImage: "",
-    overlayImage: ""
+    regionName: 'Desert',
+    markerIcon: '/static/images/regions/desert-region.svg',
+    markerCoordinates: { lat: 37.19733874, lng: -113.896597 },
+    regionImage: '',
+    overlayImage: ''
   }
 ];
 
